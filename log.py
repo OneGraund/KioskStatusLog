661c4f924b737923dbcc7d09fee59a632511d6b1
OneGraund <vovangaunder@gmail.com>
2023-02-21 13:33:38 +0100

    Continue working on good sheets data stroring

--- log.py
+++ log.py
@@ -1,6 +1,7 @@
 import gspread
 import time
 import string
+import collections
 from datetime import datetime
 
 
@@ -8,7 +9,8 @@ class File:
     def __init__(self, fname, location='.'):
         self.location = location
         self.fname = fname
-        self.buff = None
+        with open(f'{self.location}\{self.fname}', mode='r') as f:
+            self.buff = f.read().split('\n')
 
     def get_all_logs_as_dict(self):
         with open(f'{self.location}\{self.fname}', mode='r') as f:
@@ -38,14 +40,20 @@ class File:
             f.write(f'{d} {t} {s}')
         print(f'[FILE UPDATE] [NEW ROW ADDED]\n\t[DETAILS] {d} {t} {s}')
 
-
     def pull_logs_from_to(self, fr, to):
         pass
 
+    def get_last_day(self):
+        return int((self.buff[len(self.buff)-1]).split('.')[0])
+
 
 class Sheet:
+    alphabet = list(string.ascii_uppercase)
+    for i in range(26):
+        alphabet.append('A' + alphabet[i])
     def __init__(self, sh_name, wks_name):
-        for i in range(0,4):
+        self.wks_name = wks_name
+        for i in range(0, 4):
             try:
                 sa = gspread.service_account()
                 connection_error = None
@@ -70,54 +78,48 @@ class Sheet:
             start_date = wks_name.split(' ')[3]
             end_date = wks_name.split(' ')[5]
             count = int(end_date.split('.')[0])
-            self.wks = self.sh.add_worksheet(title=wks_name, rows=1441,cols=count+1)
+            self.wks = self.sh.add_worksheet(title=wks_name, rows=1441, cols=count)
             top_row = ['Time/Date']
-            for col in range(count+1):
-                top_row.append(f"{col+1}.{wks_name.split(' ')[3].split('.')[1]}."
+            for col in range(count):
+                top_row.append(f"{col + 1}.{wks_name.split(' ')[3].split('.')[1]}."
                                f"{wks_name.split(' ')[3].split('.')[2]}")
             first_col = []
             for i in range(24):
                 for u in range(60):
-                    if i<10:
-                        hour = '0'+str(i)
+                    if i < 10:
+                        hour = '0' + str(i)
                     else:
                         hour = i
-                    if u<10:
-                        min = '0'+str(u)
+                    if u < 10:
+                        min = '0' + str(u)
                     else:
                         min = u
                     first_col.append([f'{hour}:{min}'])
-            alphabet = list(string.ascii_uppercase)
-            for i in range(26):
-                alphabet.append('A' + alphabet[i])
-            self.wks.update(f'A1:{alphabet[count + 1]}1', [top_row])
-            self.wks.update(f'A2:A{len(first_col)+1}', first_col)
-        print(f'[CONNECTION] Successfully connected to {wks_name} at {sh_name}')
-        self.buff = self.wks.get(f'A1:C{self.wks.row_count - 1}')
+            self.wks.update(f'A1:{Sheet.alphabet[count + 1]}1', [top_row])
+            self.wks.update(f'A2:A{len(first_col) + 1}', first_col)
+        print(f"[CONNECTION] Successfully connected to '{wks_name} at {sh_name}'")
+        self.buff = self.wks.get(f"A1:{Sheet.alphabet[int(wks_name.split(' ')[5].split('.')[0])]}"
+                                 f"{self.wks.row_count}")
 
     def get_all_logs_as_dict(self):
-        logs = {}
-        print(f'[ROW COUNT] Based on len of self.buff there are {len(self.buff)-1} rows')
-        if len(self.buff)-1==0:
-            logs = {
-                'Date': [],
-                'Time': [],
-                'Status': []
-            }
-            return logs
-        buff = self.buff
-        header = []
-        for elem in buff[0]:
-            header.append(elem)
-        for col in range(len(header)):
-            for row in range(self.wks.row_count-1):
-                if row == 0:
-                    logs[header[col]] = [buff[row + 1][col]]
-                else:
-                    try:
-                        logs[header[col]].append(buff[row + 1][col])
-                    except:
-                        pass
+        # See until which point of time does the last day have logs in them
+        logs = {
+            'Date': [],
+            'Time': [],
+            'Status': []
+        }
+        for u in range(len(self.buff[1])-1):
+            for i in range(1440):
+                try:
+                    logs['Status'].append(self.buff[i + 1][u + 1])
+                    logs['Date'].append(f"{u+1}.{self.wks_name.split('.')[1]}."
+                                        f"{self.wks_name.split(' ')[3].split('.')[2]}")
+                    logs['Time'].append(self.buff[i+1][0])
+                    missing_data_error = None
+                except Exception as e:
+                    missing_data_error = str(e)
+                if missing_data_error:
+                    return logs
         return logs
 
     def update_sheet_buff(self):
@@ -126,48 +128,57 @@ class Sheet:
     def get_last_log_time(self):
         pass
 
-
-def update_sheet_from_file(sh, f):
-    to_upd_ids = compare_file_and_sheet(sh, f)
-    if to_upd_ids:
-        starting_row = to_upd_ids[0]
-        ending_row = to_upd_ids[len(to_upd_ids)-1]
-        buffer = []
-        file_logs = f.get_all_logs_as_dict()
-        for row in range(starting_row, ending_row+1):
-            tmp = []
-            tmp.append(file_logs['Date'][row])
-            tmp.append(file_logs['Time'][row])
-            tmp.append(file_logs['Status'][row])
-            buffer.append(tmp)
-        sh.wks.update(f'A{len(sh.buff)+1}:C{len(sh.buff)+len(to_upd_ids)}', buffer)
-    else:
-        return '[UPDATED] Worksheet is up to date with local file ...'
+    def update_sheet_from_file(self, f):
+        to_upd_ids = compare_file_and_sheet(self, f)
+        sh_logs = self.get_all_logs_as_dict()
+        f_logs = self.get_all_logs_as_dict()
+        if to_upd_ids:
+            days_to_upd = f.get_last_day()
+            to_upd=[]
+            for day in range(1, days_to_upd+1):
+                to_upd = []
+                if day<10:
+                    day_as_str = f"0{day}.{self.wks_name.split('.')[1]}." \
+                                 f"{self.wks_name.split(' ')[3].split('.')[2]}"
+                else:
+                    day_as_str = f"0{day}.{self.wks_name.split('.')[1]}." \
+                                 f"{self.wks_name.split(' ')[3].split('.')[2]}"
+                print(collections.Counter(sh_logs['Date']))
+                print(collections.Counter(f_logs['Date']))
+                print(day_as_str)
+                print(collections.Counter(sh_logs['Date'])[day_as_str])
+                print(collections.Counter(f_logs['Date'])[day_as_str])
+                if collections.Counter(sh_logs['Date'])[day_as_str]!=collections.Counter(f_logs['Date'])[day_as_str]:
+                    for i in range(to_upd_ids[1441*day], len(f.buff)%1441):
+                        to_upd.append([(f.buff)[i].split(' ')[2]])
+                    print(to_upd_ids)
+                    self.wks.update(f"{Sheet.alphabet[day]}{to_upd_ids[0] + 2 + 1441*(day-1)}:{len(f.buff) + 1 - 1441*day}", to_upd)
+        else:
+            return '[UPDATED] Worksheet is up to date with local file ...'
 
 
 def compare_file_and_sheet(sh, f):
     """Sheet is missing this rows for 02.18 15:13, 02.18 15:14, 02.18 15:15"""
     sh_d = sh.get_all_logs_as_dict()
-    print(f'[SH_D] {sh_d}')
     f_d = f.get_all_logs_as_dict()
     if sh_d['Time'] != []:
-        if len(sh_d['Time'])!=len(sh_d['Date']) or len(sh_d['Date'])!=len(sh_d['Status']):
+        if len(sh_d['Time']) != len(sh_d['Date']) or len(sh_d['Date']) != len(sh_d['Status']):
             print('[ERROR] log.py compare_file_and_sheet at line 80. Sheet data corrupted...')
             return None
-        if len(f_d['Time'])!=len(f_d['Date']) or len(f_d['Date'])!=len(f_d['Status']):
+        if len(f_d['Time']) != len(f_d['Date']) or len(f_d['Date']) != len(f_d['Status']):
             print('[ERROR] log.py compare_file_and_sheet at line 80. File data corrupted...')
             return None
-    dif = len(f_d['Time'])-len(sh_d['Time'])
-    print(f"[LEN F_D] {len(f_d['Time'])} with \n\t[F_D] {f_d}\n[LEN SH_D] {len(sh_d['Time'])} with"
-          f"\n\t[SH_D] {sh_d}")
+    dif = len(f_d['Time']) - len(sh_d['Time'])
     missing_ids = []
     message = f'[DIFFERENCE SHEET FILE] Spreadsheet is missing {dif} statuses for this date and time:'
     for i in range(dif):
-        message += f"\n\t[{f_d['Date'][len(sh_d['Date'])+i]}] [{f_d['Time'][len(sh_d['Time'])+i]}]" \
-                   f" {f_d['Status'][len(sh_d['Status'])+i]}"
-        missing_ids.append(len(sh_d['Date'])+i)
+        message += f"\n\t[{f_d['Date'][len(sh_d['Date']) + i]}] [{f_d['Time'][len(sh_d['Time']) + i]}]" \
+                   f" {f_d['Status'][len(sh_d['Status']) + i]}"
+        missing_ids.append(len(sh_d['Date']) + i)
     print(message)
     return missing_ids
 
+
 sh = Sheet('KioskStatuses_MH', '[Kiosk 1] From 01.03.2023 to 31.03.2023')
 f = File('Kiosk1.txt')
+print(sh.update_sheet_from_file(f))
